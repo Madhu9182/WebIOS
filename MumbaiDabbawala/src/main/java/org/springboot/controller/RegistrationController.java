b6bf51b4cb965f341dade67da1162471679f6c4c
Madhu9182 <96116561+Madhu9182@users.noreply.github.com>
2022-01-31 15:37:25 +0530

    Add files via upload

---
+++ MumbaiDabbawala/src/main/java/org/springboot/controller/RegistrationController.java
@@ -0,0 +1,79 @@
+package org.springboot.controller;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springboot.exception.ResourceNotFoundException;
+import org.springboot.model.RegistrationForm;
+import org.springboot.repository.RegistrationRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // It is used to build rest API
+@RequestMapping("/api") // It is used to map web request
+public class RegistrationController {
+	//Call my http methods -- get, post, put, delete
+	@Autowired//used for dependency injection
+	private RegistrationRepository registrationRepository;
+
+	//get registrationform information(view)
+	@GetMapping("registrationform")
+	public List<RegistrationForm> getRegistrationForm(){
+		return this.registrationRepository.findAll();  // To Fetch all the details
+	}
+
+	//get information with the help of Id(retrieve)
+	@GetMapping("/registrationform/{id}")
+	public ResponseEntity<RegistrationForm> getRegistrationById(@PathVariable(value="id") Long userId) throws ResourceNotFoundException{
+		RegistrationForm registration = registrationRepository.findById(userId).orElseThrow(() ->
+		new ResourceNotFoundException("Registration not found for this id :: " + userId));
+		return ResponseEntity.ok().body(registration);
+	}
+
+	//Save registration form
+	@PostMapping("registrationform")
+	public RegistrationForm createRegistrationForm(@RequestBody RegistrationForm registrationform) {
+		return this.registrationRepository.save(registrationform); 
+	}
+
+	//update the registration information
+	@PutMapping("/registrationform/{id}")
+	public ResponseEntity<RegistrationForm> getRegistrationById
+	(@PathVariable(value="id")Long userId, @Validated @RequestBody RegistrationForm registrationDetails) throws ResourceNotFoundException{
+		RegistrationForm registration = registrationRepository.findById(userId).orElseThrow(() ->
+		new ResourceNotFoundException("Registration not found for this id :: " + userId));
+
+		//set and get values by using getter and setter methods
+	//	registration.setUserId(registrationDetails.getUserId());
+		registration.setFirstname(registrationDetails.getFirstname());
+		registration.setLastname(registrationDetails.getLastname());
+		registration.setGender(registrationDetails.getGender());
+		registration.setEmail(registrationDetails.getEmail());
+		registration.setContactno(registrationDetails.getContactno());
+		registration.setPassword(registrationDetails.getPassword());
+
+		return ResponseEntity.ok(this.registrationRepository.save(registration));
+	}
+
+	//delete the information
+	@DeleteMapping("/registrationform/{id}")
+	public Map<String, Boolean> deleteRegistration(@PathVariable(value="id")Long userId)throws ResourceNotFoundException{
+		RegistrationForm registration = registrationRepository.findById(userId).orElseThrow(() ->
+		new ResourceNotFoundException("Registration is not found by that id :: " + userId));
+
+		this.registrationRepository.delete(registration);
+		Map<String, Boolean> response = new HashMap<>();
+		response.put("deleted",Boolean.TRUE);
+		return response;   // To delete the particular data 
+	}
+}
---
+++ MumbaiDabbawala/src/main/java/org/springboot/model/RegistrationForm.java
@@ -0,0 +1,94 @@
+package org.springboot.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="Registration")
+public class RegistrationForm {
+	
+	//Registration info ----> userId,first name,last name,gender,email,contact no, 
+	//To make a primary key we will use @Id
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	//	@Column(name="UserId")
+	private long userId;
+	@Column(name="First_Name")
+	private String firstname;
+	@Column(name="Last_Name")
+	private String lastname;
+	@Column(name="Gender")
+	private String gender;
+	@Column(name="Email")
+	private String email;
+	@Column(name="Contact_No")
+	private String contactno;
+	@Column(name="Password")
+	private String password;
+
+	//Parameterized constructors to store the data
+	public RegistrationForm(long userId, String firstname, String lastname, String gender, String email,
+			String contactno, String password) {
+		super();
+		this.userId = userId;
+		this.firstname = firstname;
+		this.lastname = lastname;
+		this.gender = gender;
+		this.email = email;
+		this.contactno = contactno;
+		this.password = password;
+	}
+	
+	//Default constructor
+	public RegistrationForm() {
+		super();
+	}
+	
+	//Getter and Setter Methods to set the data and get the data
+	public long getUserId() {
+		return userId;
+	}
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getContactno() {
+		return contactno;
+	}
+	public void setContactno(String contactno) {
+		this.contactno = contactno;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
